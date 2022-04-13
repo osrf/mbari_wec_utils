df9b838ba7f8008731ff106f471b2bd076a88ac7
Michael Anderson <anderson@mbari.org>
2022-04-13 16:29:20 -0700

    satisfy colcon test

--- buoy_msgs/interface.py
+++ buoy_msgs/interface.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Open Source Robotics Foundation, Inc. and Monterey Bay Aquarium Research Institute
 #
-# Licensed under the Apache License, Version 2.0 (the "License")
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 import rclpy
 from rclpy.node import Node
