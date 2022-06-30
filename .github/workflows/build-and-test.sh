f927fe5466144ea381462c664f98999c7b179ee1
Louise Poubel <louise@openrobotics.org>
2022-06-30 12:12:52 -0700

    Fix syntax
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -14,16 +14,15 @@ apt install -qq -y lsb-release wget curl build-essential
 echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt-get update -qq
-apt-get install -y git \
-                   python3-colcon-common-extensions \
-                   python3-rosdep
-                   build-essential \
+apt-get install -y build-essential \
                    cmake \
                    git \
+                   python3-colcon-common-extensions \
                    python3-colcon-common-extensions \
                    python3-flake8 \
                    python3-pip \
                    python3-pytest-cov \
+                   python3-rosdep \
                    python3-rosdep \
                    python3-setuptools \
                    python3-vcstool \
