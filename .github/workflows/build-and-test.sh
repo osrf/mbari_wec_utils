182c140581d0f371332c20dd93865ee2c38f6f0b
Louise Poubel <louise@openrobotics.org>
2022-07-29 21:50:06 -0700

    Use tros2-testing on CI
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -11,7 +11,7 @@ mkdir -p $COLCON_WS_SRC
 apt update -qq
 apt install -qq -y lsb-release wget curl build-essential
 
-echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list
+echo "deb http://packages.ros.org/ros2-testing/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-testing.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt-get update -qq
 apt-get install -y build-essential \
