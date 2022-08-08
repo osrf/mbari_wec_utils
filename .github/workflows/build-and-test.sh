f1b64f330f593b6d9a5a6401b14b2c19ad46a0ad
Louise Poubel <louise@openrobotics.org>
2022-08-08 14:02:33 -0700

    Install flake8 plugins for testing  (#14)
    * Install flake8 plugins for testing
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * apt tools and note about Jammy
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * Fix syntax
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * Use tros2-testing on CI
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -11,12 +11,36 @@ mkdir -p $COLCON_WS_SRC
 apt update -qq
 apt install -qq -y lsb-release wget curl build-essential
 
-echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list
+echo "deb http://packages.ros.org/ros2-testing/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-testing.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt-get update -qq
-apt-get install -y git \
+apt-get install -y build-essential \
+                   cmake \
+                   git \
                    python3-colcon-common-extensions \
-                   python3-rosdep
+                   python3-colcon-common-extensions \
+                   python3-flake8 \
+                   python3-pip \
+                   python3-pytest-cov \
+                   python3-rosdep \
+                   python3-rosdep \
+                   python3-setuptools \
+                   python3-vcstool \
+                   wget
+# TODO: get from apt when upgrading to Jammy
+python3 -m pip install -U \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest \
+  setuptools
 
 cd $COLCON_WS_SRC
 cp -r $GITHUB_WORKSPACE $COLCON_WS_SRC
