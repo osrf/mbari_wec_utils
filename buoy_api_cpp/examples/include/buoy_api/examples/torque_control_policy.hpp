fbb23b102e5e613439f3b349aaa218e601873afd
Michael Anderson <anderson@mbari.org>
2022-09-05 14:56:51 -0700

    fix splinter-related issues with torque policy
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- buoy_api_cpp/examples/include/buoy_api/examples/torque_control_policy.hpp
+++ buoy_api_cpp/examples/include/buoy_api/examples/torque_control_policy.hpp
@@ -21,27 +21,31 @@
 
 #include <buoy_api/examples/torque_controller.hpp>
 
-#include <vector>
-#include <algorithm>
-
 // interp1d for rpm->winding current
-#include "JustInterp/JustInterp.hpp"
+#include <splinter_ros/splinter1d.hpp>
+
+#include <algorithm>
+#include <memory>
+#include <vector>
 
 
 struct PBTorqueControlPolicy
 {
-  float Torque_constant;  // N-m/Amps
-  std::vector<float> N_Spec;  // RPM
-  std::vector<float> Torque_Spec;  // N-m
-  std::vector<float> I_Spec;  // Amps
-  JustInterp::LinearInterpolator<float> interp1d;
+  double Torque_constant;  // N-m/Amps
+  std::vector<double> N_Spec;  // RPM
+  std::vector<double> Torque_Spec;  // N-m
+  std::vector<double> I_Spec;  // Amps
+
+  // interpolator for rpm -> winding current
+  splinter_ros::Splinter1d winding_current;
 
   PBTorqueControlPolicy()
   : Torque_constant(0.438F),
     N_Spec{0.0F, 300.0F, 600.0F, 1000.0F, 1700.0F, 4400.0F, 6790.0F},
     Torque_Spec{0.0F, 0.0F, 0.8F, 2.9F, 5.6F, 9.8F, 16.6F},   // Matches old boost converter targets
                                                               // that have been deployed.
-    I_Spec(Torque_Spec.size(), 0.0F)
+    I_Spec(Torque_Spec.size(), 0.0F),
+    winding_current(N_Spec, I_Spec)
   {
     update_params();
   }
@@ -51,24 +55,23 @@ struct PBTorqueControlPolicy
     std::transform(
       Torque_Spec.cbegin(), Torque_Spec.cend(),
       I_Spec.begin(),
-      [tc = Torque_constant](const float & ts) {return ts / tc;});
+      [tc = Torque_constant](const double & ts) {return ts / tc;});
 
-    interp1d.SetData(N_Spec, I_Spec);
+    winding_current.update(N_Spec, I_Spec);
   }
 
-  // TODO(andermi): find a way to make this generic...
-  float WindingCurrentTarget(
-    const float & rpm,
-    const float & scale_factor,
-    const float & retract_factor)
+  double WindingCurrentTarget(
+    const double & rpm,
+    const double & scale_factor,
+    const double & retract_factor)
   {
-    float N = fabs(rpm);
-    float I = 0.0F;
+    double N = fabs(rpm);
+    double I = 0.0F;
 
     if (N >= N_Spec.back()) {
       I = I_Spec.back();
     } else {
-      I = interp1d(N);
+      I = winding_current.eval(N);
     }
 
     I *= scale_factor;
@@ -87,23 +90,24 @@ std::ostream & operator<<(std::ostream & os, const PBTorqueControlPolicy & polic
   os << "\tTorque_constant: " << policy.Torque_constant << std::endl;
 
   os << "\tN_Spec: " << std::flush;
-  std::copy(policy.N_Spec.cbegin(), policy.N_Spec.cend(), std::ostream_iterator<float>(os, ","));
+  std::copy(policy.N_Spec.cbegin(), policy.N_Spec.cend(), std::ostream_iterator<double>(os, ","));
   os << "\b \b" << std::endl;
 
   os << "\tTorque_Spec: " << std::flush;
   std::copy(
     policy.Torque_Spec.cbegin(),
     policy.Torque_Spec.cend(),
-    std::ostream_iterator<float>(os, ","));
+    std::ostream_iterator<double>(os, ","));
   os << "\b \b" << std::endl;
 
   os << "\tI_Spec: " << std::flush;
-  std::copy(policy.I_Spec.cbegin(), policy.I_Spec.cend(), std::ostream_iterator<float>(os, ","));
+  std::copy(policy.I_Spec.cbegin(), policy.I_Spec.cend(), std::ostream_iterator<double>(os, ","));
   os << "\b \b" << std::endl;
 
   return os;
 }
 
+#ifndef POLICY_ONLY
 
 void PBTorqueController::set_params()
 {
@@ -127,5 +131,6 @@ void PBTorqueController::set_params()
   policy_->update_params();
   RCLCPP_INFO_STREAM(rclcpp::get_logger(this->get_name()), *policy_);
 }
+#endif  // POLICY_ONLY
 
 #endif  // BUOY_API__EXAMPLES__TORQUE_CONTROL_POLICY_HPP_
