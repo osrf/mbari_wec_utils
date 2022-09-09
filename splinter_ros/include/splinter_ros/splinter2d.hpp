acd621f5abf1ae24651efb2bd02f8b35af1428e4
andermi <anderson@mbari.org>
2022-09-08 18:25:48 -0700

    Use splinter in separate package (#28)
    * pump should be in minutes
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * pbcmd pump command in minutes
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * move python stuff out of buoy_examples; fix examples
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * move cpp stuff out of buoy_examples
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * fix linters
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * bring changes over from buoy for bias damping
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * ignore temps
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * fix splinter-related issues with torque policy
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * move splinter here from buoy_sim
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * WIP: fixing splinter and adding mpl2 to ament_copyright
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * WIP; fix mpl2 license/contrib; add license headers to splinter_ros files
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * put torque_controller back; cleanup splinter cmake
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * linters
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * remove debug code
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * make CI source workspace before test
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * Break splinter into _vendor and _ros packages (#29)
    
    * Break splinter into _vendor and _ros packages
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * fix some cmake build issues
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    Co-authored-by: Michael Anderson <anderson@mbari.org>
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    Co-authored-by: Michael Carroll <michael@openrobotics.org>

---
+++ splinter_ros/include/splinter_ros/splinter2d.hpp
@@ -0,0 +1,46 @@
+// Copyright 2022 Open Source Robotics Foundation, Inc. and Monterey Bay Aquarium Research Institute
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SPLINTER_ROS__SPLINTER2D_HPP_
+#define SPLINTER_ROS__SPLINTER2D_HPP_
+
+#include <memory>
+#include <vector>
+
+
+namespace splinter_ros
+{
+struct Splinter2dImpl;
+class Splinter2d
+{
+public:
+  explicit Splinter2d(
+    const std::vector<double> & x,
+    const std::vector<double> & y,
+    const std::vector<std::vector<double>> & z);
+
+  void update(
+    const std::vector<double> & x,
+    const std::vector<double> & y,
+    const std::vector<std::vector<double>> & z);
+
+  double eval(
+    const double & x,
+    const double & y) const;
+
+private:
+  std::shared_ptr<Splinter2dImpl> impl_;
+};
+}  // namespace splinter_ros
+#endif  // SPLINTER_ROS__SPLINTER2D_HPP_
