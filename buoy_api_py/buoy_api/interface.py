e13509f4c2c621101817a010bcf18b452b6e8279
andermi <anderson@mbari.org>
2023-02-03 15:10:15 -0800

    Revert "update prints for finding all services (#40)" (#41)
    * Revert "update prints for finding all services (#40)"
    
    This reverts commit 8cd1982ea0cb960cc9cf5e1e80abcba157c6bcd0.
    
    * fix quotations on print

--- buoy_api_py/buoy_api/interface.py
+++ buoy_api_py/buoy_api/interface.py
@@ -154,7 +154,7 @@ class Interface(Node):
             if not found and wait_for_services:
                 while rclpy.ok() and not self.wait_for_services():
                     pass
-            self.get_logger().info("Found all required services.")
+            self.get_logger().info('Found all required services.')
 
     def wait_for_services(self):
         # TODO(andermi)
